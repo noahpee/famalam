7108cb162e4c93c0856b48ab2716d32ba4d350c9
noah <noah@noahs-MacBook-Pro.local>
2023-12-11 13:44:10 +0000

    d

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ var buttonElement = document.getElementById("Butt");
             // Display an alert when the button is clicked
             alert("sent");
         });
+
+        var clickerElement = document.getElementById("clicker");
+
+        // Add a click event listener
+        clickerElement.addEventListener("click", function() {
+            // Display an alert when the button is clicked
+            alert("sent");
+        });
